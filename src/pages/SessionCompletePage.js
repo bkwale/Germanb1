4d75c10f5a1232f6b419455eb9c157ac4f1833bc
bkwale <169000509+bkwale@users.noreply.github.com>
2026-01-25 15:14:48 +0000

    Remove unused useParams import

--- src/pages/SessionCompletePage.js
+++ src/pages/SessionCompletePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useApp } from '../contexts/AppContext';
 import './SessionCompletePage.css';
 
